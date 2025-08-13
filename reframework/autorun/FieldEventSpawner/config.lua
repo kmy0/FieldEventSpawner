e1b0f87e3513b7a7bb965ebdb6b894c68d7207cd
kmy0 <93156259+kmy0@users.noreply.github.com>
2025-08-13 05:21:01 +0200

    bump version to 0.1.1

--- reframework/autorun/FieldEventSpawner/config.lua
+++ reframework/autorun/FieldEventSpawner/config.lua
@@ -82,7 +82,7 @@ local util = require("FieldEventSpawner.util")
 ---@class Config
 local this = {}
 
-this.version = "0.1.0"
+this.version = "0.1.1"
 this.name = "FieldEventSpawner"
 this.config_path = this.name .. "/config.json"
 this.cache_path = this.name .. "/cache.json"
