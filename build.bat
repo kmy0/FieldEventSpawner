8fbbaabb4b4f3c94870b6fd0120e609fdbcb6414
kmy0 <kmygat@gmail.com>
2026-08-06 04:02:17 +0200

    update build file

--- build.bat
+++ build.bat
@@ -8,3 +8,4 @@ md bin\reframework\plugins
 robocopy reframework bin\reframework /mir
 robocopy deps\fes_util\bin bin\reframework\plugins fes_util.dll
 tar -a -cf FieldEventSpawner.zip -C bin reframework
+tar -a -cf AllMonstersAllMaps.zip natives
