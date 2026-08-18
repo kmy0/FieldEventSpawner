56de28db294de069cbd88dd79c6c477adace88ba
kmy0 <kmygat@gmail.com>
2026-08-18 08:14:01 +0200

    cleanup unused types

--- typedef/ace/via.lua
+++ typedef/ace/via.lua
@@ -8,16 +8,14 @@
 ---@class via.Behavior : via.Component
 ---@class via.gui.TransformObject : via.gui.PlayObject
 ---@class via.gui.PlayObject : via.clr.ManagedObject
+---@---@class via.Scene : via.clr.ManagedObject
 
 ---@class via.Size
 ---@field w System.Single
 ---@field h System.Single
----@
+
 ---@class via.GameObject : via.clr.ManagedObject
 ---@field destroy fun(self: via.GameObject, object: via.GameObject)
-
----@class via.Scene : via.clr.ManagedObject
----@field get_FrameCount fun(self: via.Scene): System.UInt32
 
 ---@class via.SceneView : via.gui.TransformObject
 ---@field get_WindowSize fun(self: via.SceneView): via.Size
